cf93e4b8922c9e7d9d7ca06f8d0178a01ee8e397
Yeoeun Yang <yyang78077@naver.com>
2025-05-29 21:45:26 +0900

    init project

---
+++ src/main/java/com/ureca/uplait/UPlaitApplication.java
@@ -0,0 +1,13 @@
+package com.ureca.uplait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UPlaitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UPlaitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ureca/uplait/UPlaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ureca.uplait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UPlaitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
