3ee3507e51cc3c5e6ce2e96318bbdf2b4dace37d
임동준 <127932430+djlim00@users.noreply.github.com>
2025-06-06 12:13:10 +0900

    feat: BaseEntity, User 엔티티 및 공통 enum(Gender, Role, Status) 정의

---
+++ src/main/java/com/ureca/uplait/domain/user/entity/User.java
@@ -0,0 +1,67 @@
+package com.ureca.uplait.domain.user.entity;
+
+import com.ureca.uplait.domain.chat.entity.ChatLog;
+import com.ureca.uplait.domain.contract.entity.Contract;
+import com.ureca.uplait.domain.review.entity.Review;
+import com.ureca.uplait.domain.user.enums.Gender;
+import com.ureca.uplait.domain.user.enums.Role;
+import com.ureca.uplait.domain.user.enums.Status;
+import com.ureca.uplait.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "kakao_id", nullable = false)
+    private String kakaoId;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(name = "ad_agree", nullable = false)
+    private Boolean adAgree;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Bookmark> bookmarks;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Contract> contracts;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Interest> interests;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<ChatLog> chatLogs;
+}
---
+++ src/main/java/com/ureca/uplait/domain/user/enums/Role.java
@@ -0,0 +1,6 @@
+package com.ureca.uplait.domain.user.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
