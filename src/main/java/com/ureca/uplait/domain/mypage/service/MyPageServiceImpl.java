3402cc390f52013d75c74fb51aba01b7ab2a33e8
yoon_je <etoile0626@naver.com>
2025-06-09 10:43:09 +0900

    update: mypage dto 수정

--- src/main/java/com/ureca/uplait/domain/mypage/service/MyPageServiceImpl.java
+++ src/main/java/com/ureca/uplait/domain/mypage/service/MyPageServiceImpl.java
@@ -1,12 +1,18 @@
 package com.ureca.uplait.domain.mypage.service;
 
 import com.ureca.uplait.domain.mypage.dto.MyPageResponse;
+import com.ureca.uplait.domain.mypage.dto.MyReviewsResponse;
 
 public class MyPageServiceImpl implements MyPageService {
 
     @Override
     public MyPageResponse getMyPage(Long userId) {
 
+        return null;
+    }
+
+    @Override
+    public MyReviewsResponse getMyReview(Long id) {
         return null;
     }
 }
