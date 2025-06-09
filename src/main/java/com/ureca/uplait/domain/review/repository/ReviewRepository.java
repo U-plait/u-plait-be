f798e147187411c8c05a1f5e35f7468fd4e2d0b6
yoon_je <etoile0626@naver.com>
2025-06-09 11:14:48 +0900

    feat: 마이페이지에서 리뷰 정보 가져오는 중

---
+++ src/main/java/com/ureca/uplait/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.ureca.uplait.domain.review.repository;
+
+import com.ureca.uplait.domain.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Optional<Review> findByUserId(Long userId);
+}
