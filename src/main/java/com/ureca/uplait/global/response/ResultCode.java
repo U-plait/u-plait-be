13d2188ac8cf87af34921e32436641612aecb584
hayong39 <yia0524_hy@naver.com>
2025-06-09 11:05:34 +0900

    refactor: 응답형식 변경

--- src/main/java/com/ureca/uplait/global/response/ResultCode.java
+++ src/main/java/com/ureca/uplait/global/response/ResultCode.java
@@ -2,6 +2,8 @@ package com.ureca.uplait.global.response;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 
 @Getter
@@ -10,15 +12,18 @@ public enum ResultCode {
     // 글로벌 1000번대
     SUCCESS(HttpStatus.OK, 0, "정상 처리 되었습니다."),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, 1000, "잘못된 입력값입니다."),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 1001, "유효하지 않은 토큰입니다."), // 로그인 관련
-    REFRESH_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, 1002, "Refresh Token이 필요합니다."), // 로그인 관련
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, 1003, "사용자를 찾을 수 없습니다."),
-    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, 1004, "Refresh Token이 만료되었습니다."), // 로그인 관련
-    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, 1005, "Access Token이 만료되었습니다."), // 로그인 관련
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, 1006, "인증이 필요합니다."),
     FORBIDDEN(HttpStatus.FORBIDDEN, 1007, "권한이 없는 사용자입니다."),
 
-    // 2000번대
+
+    // 2000번대 (로그인 관련)
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, 2000, "유효하지 않은 토큰입니다."),
+    REFRESH_TOKEN_REQUIRED(HttpStatus.UNAUTHORIZED, 2001, "Refresh Token이 필요합니다."),
+    REFRESH_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, 2002, "Refresh Token이 만료되었습니다."),
+    ACCESS_TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, 2003, "Access Token이 만료되었습니다."),
+    REISSUE_SUCCESS(HttpStatus.OK, 2004, "토큰 재발급에 성공했습니다. "),
+    LOGOUT_SUCCESS(HttpStatus.OK, 2005, "로그아웃에 성공했습니다.")
 
     ;
 
