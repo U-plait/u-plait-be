98555a846bcfdfc650763cf56b26d1f5ca242429
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-10 11:39:51 +0900

    feat: 금칙어 수정 기능 삭제

--- src/main/java/com/ureca/uplait/domain/banword/entity/BanWord.java
+++ src/main/java/com/ureca/uplait/domain/banword/entity/BanWord.java
@@ -19,8 +19,4 @@ public class BanWord extends BaseEntity {
 
     @Column(name = "ban_word", nullable = false, length = 20)
     private String banWord;
-
-    public void updateBanWord(String word) {
-        this.banWord = word;
-    }
 }
