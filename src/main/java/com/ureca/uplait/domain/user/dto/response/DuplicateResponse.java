7f37e5a72419fe0030d81275a681bab889578e5d
hayong39 <yia0524_hy@naver.com>
2025-06-10 16:46:37 +0900

    refactor: lombok으로 처리

--- src/main/java/com/ureca/uplait/domain/user/dto/response/DuplicateResponse.java
+++ src/main/java/com/ureca/uplait/domain/user/dto/response/DuplicateResponse.java
@@ -1,16 +1,15 @@
 package com.ureca.uplait.domain.user.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 @Schema(description = "중복 여부 담는 DTO")
 public class DuplicateResponse {
 
 	@Schema(description = "중복 여부", example = "true")
 	boolean duplicated;
 
-	public DuplicateResponse(boolean duplicated) {
-		this.duplicated = duplicated;
-	}
 }
