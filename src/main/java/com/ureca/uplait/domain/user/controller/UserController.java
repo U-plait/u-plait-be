56ef86026e4721fae7430fa925e3ba11914cdb2f
hayong39 <yia0524_hy@naver.com>
2025-06-10 16:47:00 +0900

    fix: summary와 description 변경

--- src/main/java/com/ureca/uplait/domain/user/controller/UserController.java
+++ src/main/java/com/ureca/uplait/domain/user/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.ureca.uplait.domain.user.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ureca.uplait.domain.user.dto.response.DuplicateResponse;
 import com.ureca.uplait.domain.user.dto.request.ExtraInfoRequest;
-import com.ureca.uplait.domain.user.dto.response.UserNameResponse;
 import com.ureca.uplait.domain.user.entity.User;
 import com.ureca.uplait.domain.user.service.UserService;
 import com.ureca.uplait.global.response.CommonResponse;
@@ -30,22 +28,15 @@ public class UserController {
 
 	private final UserService userService;
 
-	@GetMapping("/me")
-	@Operation(description="네비게이션바 API", summary="로그인한 유저의 이름을 반환한다.")
-	public ResponseEntity<CommonResponse<UserNameResponse>> navbar(@AuthenticationPrincipal User user) {
-		UserNameResponse result = new UserNameResponse(user.getName());
-		return ResponseEntity.ok(CommonResponse.success(result));
-	}
-
 	@PostMapping("/extra-info")
-	@Operation(description="회원가입 API", summary = "최초 카카오 로그인 이후 추가정보를 입력받는다.")
+	@Operation(summary="회원가입 API", description = "최초 카카오 로그인 이후 추가정보를 입력받는 API.")
 	public CommonResponse<Void> extraInfo(@Valid @RequestBody ExtraInfoRequest request, @AuthenticationPrincipal User user) {
 		userService.updateUserExtraInfo(request, user);
 		return new CommonResponse<>(ResultCode.SIGNUP_SUCCESS);
 	}
 
 	@GetMapping("/duplicate/phone")
-	@Operation(description="전화번호 중복검사 API", summary = "추가정보 입력 시 전화번호 중복 검사를 수행한다.")
+	@Operation(summary="전화번호 중복검사 API", description = "추가정보 입력 시 전화번호 중복 검사를 수행한다.")
 	public CommonResponse<DuplicateResponse> duplicatePhone(
 		@RequestParam("value")
 		@Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호는 010-xxxx-xxxx 형식이어야 합니다.")
@@ -56,7 +47,7 @@ public class UserController {
 	}
 
 	@GetMapping("/duplicate/email")
-	@Operation(description="이메일 중복검사 API", summary = "추가정보 입력 시 이메일 중복 검사를 수행한다.")
+	@Operation(summary="이메일 중복검사 API", description = "추가정보 입력 시 이메일 중복 검사를 수행한다.")
 	public CommonResponse<DuplicateResponse> duplicateEmail(
 		@RequestParam("value")
 		@Email(message = "올바른 이메일 형식이 아닙니다.")
@@ -66,8 +57,4 @@ public class UserController {
 		DuplicateResponse result = new DuplicateResponse(duplicated);
 		return CommonResponse.success(result);
 	}
-
-
-
-
 }
