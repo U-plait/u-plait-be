733e213b52757e3b06e8f8534d73da0223346529
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-10 11:41:04 +0900

    feat: 금칙어 등록 요청 DTO 변경

--- src/main/java/com/ureca/uplait/domain/banword/dto/request/BanWordRequest.java
+++ src/main/java/com/ureca/uplait/domain/banword/dto/request/BanWordRequest.java
@@ -1,4 +1,9 @@
 package com.ureca.uplait.domain.banword.dto.request;
 
-public record BanWordRequest(String banWord) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "금칙어 등록 요청 DTO")
+public record BanWordRequest(
+        @Schema(description = "등록할 금칙어", example = "시발")
+        String banWord) {
 }
