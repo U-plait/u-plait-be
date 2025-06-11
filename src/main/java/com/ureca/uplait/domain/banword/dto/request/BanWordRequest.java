419d3662bb3ba69699ca65cc48eafff75ff01ed1
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-11 13:48:00 +0900

    feat: banWord 유효성 검사 추가

--- src/main/java/com/ureca/uplait/domain/banword/dto/request/BanWordRequest.java
+++ src/main/java/com/ureca/uplait/domain/banword/dto/request/BanWordRequest.java
@@ -1,9 +1,11 @@
 package com.ureca.uplait.domain.banword.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "금칙어 등록 요청 DTO")
 public record BanWordRequest(
         @Schema(description = "등록할 금칙어", example = "시발")
+        @NotBlank(message = "금칙어는 공백일 수 없습니다.")
         String banWord) {
 }
