73d490d39ae4e5d123c8af56688e79cd602243e8
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-11 13:01:08 +0900

    feat: 금칙어 조회 응답 DTO 추가

---
+++ src/main/java/com/ureca/uplait/domain/banword/dto/response/BanWordResponse.java
@@ -0,0 +1,21 @@
+package com.ureca.uplait.domain.banword.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter@Setter
+@Schema(description = "금칙어 조회 응답 DTO")
+public class BanWordResponse {
+
+    @Schema(description = "금칙어 ID", example = "1")
+    private Long id;
+
+    @Schema(description = "금칙어 문자열", example = "시발")
+    private String banWord;
+
+    @Schema(description = "등록 일시", example = "2025-06-10T00:00:00")
+    private LocalDateTime createdAt;
+}
