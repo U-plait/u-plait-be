9c7858da3f215125ea5b777dcfbe833e195c52fb
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-11 17:36:40 +0900

    refactor: BanWordRequest 기본 생성자 생성

--- src/main/java/com/ureca/uplait/domain/banword/dto/request/BanWordRequest.java
+++ src/main/java/com/ureca/uplait/domain/banword/dto/request/BanWordRequest.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Schema(description = "금칙어 등록 요청 DTO")
 public class BanWordRequest {
 
