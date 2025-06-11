e4e3f0d68514b2bb7460645c6f2d688bf10d0ca5
song hyeongyu <lit3207@ajou.ac.kr>
2025-06-11 16:42:44 +0900

    feat:UP-30 AdminReview 구현

--- src/main/java/com/ureca/uplait/domain/review/repository/CustomReviewRepositoryImpl.java
+++ src/main/java/com/ureca/uplait/domain/review/repository/CustomReviewRepositoryImpl.java
@@ -1,18 +1,25 @@
 package com.ureca.uplait.domain.review.repository;
 
+import static com.ureca.uplait.domain.review.entity.QReview.review;
+
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ureca.uplait.domain.review.dto.response.AdminReviewResponse;
+import com.ureca.uplait.domain.review.entity.QReview;
 import com.ureca.uplait.domain.review.entity.Review;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
+import com.ureca.uplait.domain.user.entity.QUser;
 import java.util.List;
-
-import static com.ureca.uplait.domain.review.entity.QReview.review;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class CustomReviewRepositoryImpl implements CustomReviewRepository{
+public class CustomReviewRepositoryImpl implements CustomReviewRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
@@ -30,12 +37,47 @@ public class CustomReviewRepositoryImpl implements CustomReviewRepository{
     }
 
     private BooleanExpression ltReviewId(Long lastReviewId) {
-        if(lastReviewId == null) return null;
+        if (lastReviewId == null) {
+            return null;
+        }
         return review.id.lt(lastReviewId);
     }
 
     private BooleanExpression planIdEq(Long planId) {
-        if(planId == null) return null;
+        if (planId == null) {
+            return null;
+        }
         return review.plan.id.eq(planId);
     }
+
+    @Override
+    public Page<AdminReviewResponse> findAllReviewsForAdmin(Pageable pageable) {
+        QReview review = QReview.review;
+        QUser user = QUser.user;
+
+        List<AdminReviewResponse> content = jpaQueryFactory
+            .select(Projections.constructor(
+                AdminReviewResponse.class,
+                review.id,
+                user.name,
+                review.title,
+                review.rating,
+                review.createdAt // 여기선 LocalDateTime 그대로 넘김
+            ))
+            .from(review)
+            .leftJoin(review.user, user)
+            .orderBy(review.id.desc())
+            .offset(pageable.getOffset())
+            .limit(pageable.getPageSize())
+            .fetch();
+
+        return PageableExecutionUtils.getPage(content, pageable, () ->
+            Optional.ofNullable(
+                jpaQueryFactory.select(review.count())
+                    .from(review)
+                    .fetchOne()
+            ).orElse(0L)
+        );
+    }
+
 }
