3cddbd7334f22837a360549d8ea809d80b0f97e2
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-11 13:46:12 +0900

    feat: 금칙어 조회 페이징 기능 추가

--- src/main/java/com/ureca/uplait/domain/banword/controller/AdminBanWordController.java
+++ src/main/java/com/ureca/uplait/domain/banword/controller/AdminBanWordController.java
@@ -4,16 +4,15 @@ import com.ureca.uplait.domain.banword.dto.request.BanWordRequest;
 import com.ureca.uplait.domain.banword.dto.response.BanWordResponse;
 import com.ureca.uplait.domain.banword.service.BanWordService;
 import com.ureca.uplait.global.response.CommonResponse;
-import com.ureca.uplait.global.response.ResultCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,47 +28,50 @@ public class AdminBanWordController {
     // 금칙어 생성
     @Operation(summary = "금칙어 등록", description = "금칙어 등록 : 관리자 로그인 필요")
     @PostMapping
-    public ResponseEntity<CommonResponse<BanWordResponse>> createBanWord(
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "등록할 금칙어 요청")
-            @RequestBody BanWordRequest request) {
+    public CommonResponse<BanWordResponse> createBanWord(
+            @Parameter(description = "등록할 금칙어 요청")
+            @Valid @RequestBody BanWordRequest request) {
         BanWordResponse response = banWordService.registerBanWord(request);
-        return ResponseEntity.ok(CommonResponse.success(response));
+        return CommonResponse.success(response);
     }
 
     // 금칙어 전체 조회
     @Operation(summary = "금칙어 전체 조회", description = "금칙어 전체 조회 : 관리자 로그인 필요")
     @GetMapping
-    public ResponseEntity<CommonResponse<List<BanWordResponse>>> getAllBanWords() {
-        List<BanWordResponse> list = banWordService.getAllBanWords();
-        return ResponseEntity.ok(CommonResponse.success(list));
+    public CommonResponse<Page<BanWordResponse>> getAllBanWords(
+            @Parameter(hidden = true)
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<BanWordResponse> result= banWordService.getAllBanWords(pageable);
+        return CommonResponse.success(result);
     }
 
 
     // 금칙어 단일 삭제
     @Operation(summary = "금칙어 단일 삭제", description = "금칙어 단일 삭제 : 관리자 로그인 필요")
     @DeleteMapping("/{banwordId}")
-    public ResponseEntity<CommonResponse<Void>> deleteBanWord(
+    public CommonResponse<Void> deleteBanWord(
             @Parameter(description = "삭제할 금칙어 ID")
             @PathVariable("banwordId") Long id) {
         banWordService.deleteBanWordById(id);
-        return ResponseEntity.ok(new CommonResponse<>(ResultCode.SUCCESS));
+        return CommonResponse.success(null);
     }
 
 
     // 금칙어 일괄 삭제
     @Operation(summary = "금칙어 일괄 삭제", description = "금칙어 일괄 삭제 : 관리자 로그인 필요")
     @DeleteMapping
-    public ResponseEntity<CommonResponse<Void>> deleteBanWords(
-            @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "삭제할 금칙어 ID 목록")
+    public CommonResponse<Void> deleteBanWords(
+            @Parameter(description = "삭제할 금칙어 ID 목록")
             @RequestBody List<Long> ids) {
         banWordService.deleteBanWordsByIds(ids);
-        return ResponseEntity.ok(new CommonResponse<>(ResultCode.SUCCESS));
+        return CommonResponse.success(null);
     }
 
     // 금칙어 검색
     @Operation(summary = "금칙어 검색", description = "금칙어 검색 : 관리자 로그인 필요")
     @GetMapping("/search")
-    public ResponseEntity<CommonResponse<Page<BanWordResponse>>> searchBanWords(
+    public CommonResponse<Page<BanWordResponse>> searchBanWords(
             @Parameter(description = "검색할 키워드", required = false)
             @RequestParam(name = "keyword", required = false) String keyword,
 
@@ -77,7 +79,7 @@ public class AdminBanWordController {
             @PageableDefault(size=10, sort="createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 
         Page<BanWordResponse> result = banWordService.searchBanWords(keyword, pageable);
-        return ResponseEntity.ok(CommonResponse.success(result));
+        return CommonResponse.success(result);
     }
 }
 
