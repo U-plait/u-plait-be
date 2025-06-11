08666bbda6502d7de6314022362894ed4b350abe
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-11 15:39:04 +0900

    feat: 검색 및 페이징 기능 메서드 추가

--- src/main/java/com/ureca/uplait/domain/banword/repository/BanWordRepository.java
+++ src/main/java/com/ureca/uplait/domain/banword/repository/BanWordRepository.java
@@ -1,8 +1,11 @@
 package com.ureca.uplait.domain.banword.repository;
 
 import com.ureca.uplait.domain.banword.entity.BanWord;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BanWordRepository extends JpaRepository<BanWord, Long> {
     boolean existsByBanWord(String banWord);
+    Page<BanWord> findByBanWordContainingIgnoreCase(String keyword, Pageable pageable);
 }
