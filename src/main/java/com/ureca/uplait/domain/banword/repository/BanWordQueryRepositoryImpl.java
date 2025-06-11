97310559ceed323b341c10195dc5dc31b781516c
Heo JinHyeok <jinhyeok9290@gmail.com>
2025-06-11 13:47:33 +0900

    feat: BanWordQueryRepository findAll() 구현

--- src/main/java/com/ureca/uplait/domain/banword/repository/BanWordQueryRepositoryImpl.java
+++ src/main/java/com/ureca/uplait/domain/banword/repository/BanWordQueryRepositoryImpl.java
@@ -21,8 +21,8 @@ public class BanWordQueryRepositoryImpl implements BanWordQueryRepository {
         QBanWord banWord = QBanWord.banWord1;
 
         BooleanBuilder builder = new BooleanBuilder();
-        if (keyword != null && !keyword.isBlank()) {
-            builder.and(banWord.banWord.containsIgnoreCase(keyword));
+        if (keyword != null && !keyword.trim().isBlank()) {
+            builder.and(banWord.banWord.containsIgnoreCase(keyword.trim()));
         }
 
         List<BanWord> content = queryFactory
@@ -30,12 +30,34 @@ public class BanWordQueryRepositoryImpl implements BanWordQueryRepository {
                 .where(builder)
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
+                .orderBy(banWord.createdAt.desc())
                 .fetch();
 
         Long cnt = queryFactory
                 .select(banWord.count())
                 .from(banWord)
                 .where(builder)
+                .fetchOne();
+
+        long total = (cnt != null) ? cnt : 0L;
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<BanWord> findAll(Pageable pageable) {
+        QBanWord banWord = QBanWord.banWord1;
+
+        List<BanWord> content = queryFactory
+                .selectFrom(banWord)
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .orderBy(banWord.createdAt.desc())
+                .fetch();
+
+        Long cnt = queryFactory
+                .select(banWord.count())
+                .from(banWord)
                 .fetchOne();
 
         long total = (cnt != null) ? cnt : 0L;
