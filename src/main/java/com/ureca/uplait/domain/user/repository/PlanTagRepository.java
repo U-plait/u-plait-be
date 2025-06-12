4572010e13ecc3364cf30f00da0c83d37699baa7
Yeoeun Yang <yyang78077@naver.com>
2025-06-13 00:58:32 +0900

    feat: 문장 생성 및 FastAPI 호출 로직 추가

--- src/main/java/com/ureca/uplait/domain/user/repository/PlanTagRepository.java
+++ src/main/java/com/ureca/uplait/domain/user/repository/PlanTagRepository.java
@@ -1,7 +1,12 @@
 package com.ureca.uplait.domain.user.repository;
 
+import com.ureca.uplait.domain.plan.entity.Plan;
 import com.ureca.uplait.domain.user.entity.PlanTag;
+import com.ureca.uplait.domain.user.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlanTagRepository extends JpaRepository<PlanTag, Long> {
+    List<PlanTag> findAllByPlan(Plan plan);
 }
