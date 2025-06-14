42bbbfc7b26c827747db99e356e3fbe6bd3642f7
Yeoeun Yang <yyang78077@naver.com>
2025-06-14 19:03:58 +0900

    chore: 연관관계 수정

--- src/main/java/com/ureca/uplait/domain/token/entity/Token.java
+++ src/main/java/com/ureca/uplait/domain/token/entity/Token.java
@@ -1,25 +1,14 @@
 package com.ureca.uplait.domain.token.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.cglib.core.Local;
-
 import com.ureca.uplait.domain.user.entity.User;
 import com.ureca.uplait.global.entity.BaseEntity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name="token")
@@ -35,7 +24,7 @@ public class Token extends BaseEntity {
 	@Column(name="expiry_date", nullable = false)
 	private LocalDateTime expiryDate;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="user_id", nullable = false)
 	private User user;
 
