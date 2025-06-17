ecab682b8e443ae3640ed1d4c74f925306cead88
Yeoeun Yang <yyang78077@naver.com>
2025-06-17 17:03:43 +0900

    feat: LB 검사 API 추가

---
+++ src/main/java/com/ureca/uplait/domain/admin/controller/AdminLoadBalanceController.java
@@ -0,0 +1,13 @@
+package com.ureca.uplait.domain.admin.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminLoadBalanceController {
+    @GetMapping
+    public ResponseEntity<String> checkConnection() {
+        return ResponseEntity.ok("응답이 반환되었습니다.");
+    }
+}
