107d0e02acf6327ccb01330984952637f1b0091d
yoon_je <etoile0626@naver.com>
2025-06-18 13:33:21 +0900

    test: IPTV 요금제 관련 테스트 클래스 생성

--- src/test/java/com/ureca/uplait/domain/admin/service/AdminPlanServiceTest.java
+++ src/test/java/com/ureca/uplait/domain/admin/service/AdminPlanServiceTest.java
@@ -1,23 +1,28 @@
 package com.ureca.uplait.domain.admin.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminPlanServiceTest {
 
+    @DisplayName("IPTV 요금제 생성")
     @Test
     void createIptvPlan() {
     }
 
+    @DisplayName("IPTV 요금제 생성")
     @Test
     void updateIptvPlan() {
     }
 
+    @DisplayName("IPTV 요금제 생성")
     @Test
     void getAllIptvPlans() {
     }
 
+    @DisplayName("IPTV 요금제 생성")
     @Test
     void getTypedPlanDetail() {
     }
---
+++ src/test/java/com/ureca/uplait/domain/plan/service/PlanServiceTest.java
@@ -0,0 +1,12 @@
+package com.ureca.uplait.domain.plan.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanServiceTest {
+
+    @Test
+    void getAllIPTVPlans() {
+    }
+}
