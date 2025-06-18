64f63b5a430ee40e81d74f3c12f3e36e92076952
yoon_je <etoile0626@naver.com>
2025-06-18 15:30:34 +0900

    refactor: 중복 ResultCode 수정

--- src/main/java/com/ureca/uplait/global/response/ResultCode.java
+++ src/main/java/com/ureca/uplait/global/response/ResultCode.java
@@ -57,7 +57,7 @@ public enum ResultCode {
 
     // Tag 8000번대
     TAG_NOT_FOUND(HttpStatus.NOT_FOUND, 8000, "해당 태그를 찾을 수 없습니다."),
-    TAG_ADD_SUCCESS(HttpStatus.OK, 8000, "사용자에게 태그 정보를 입력했습니다."),
+    TAG_ADD_SUCCESS(HttpStatus.OK, 8001, "사용자에게 태그 정보를 입력했습니다."),
     ;
 
     private final HttpStatus status;
