efdc4f932096fb50c1baef1b29337ce50932df43
song hyeongyu <lit3207@ajou.ac.kr>
2025-06-19 17:26:12 +0900

    feat:UP-77 create 및 update 수정

--- src/main/java/com/ureca/uplait/domain/community/repository/PlanCommunityRepository.java
+++ src/main/java/com/ureca/uplait/domain/community/repository/PlanCommunityRepository.java
@@ -2,10 +2,12 @@ package com.ureca.uplait.domain.community.repository;
 
 import com.ureca.uplait.domain.community.entity.PlanCommunity;
 import com.ureca.uplait.domain.plan.entity.Plan;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PlanCommunityRepository extends JpaRepository<PlanCommunity, Long> {
+
     List<PlanCommunity> findAllByPlan(Plan plan);
+
+    void deleteAllByPlan(Plan plan);
 }
--- src/main/java/com/ureca/uplait/domain/plan/dto/response/InternetPlanDetailResponse.java
+++ src/main/java/com/ureca/uplait/domain/plan/dto/response/InternetPlanDetailResponse.java
@@ -2,16 +2,24 @@ package com.ureca.uplait.domain.plan.dto.response;
 
 import com.ureca.uplait.domain.plan.entity.InternetPlan;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
 @Schema(description = "인터넷 요금제 상세")
 public class InternetPlanDetailResponse extends PlanDetailResponse {
+
     @Schema(description = "인터넷 속도", example = "500M")
     private String velocity;
 
     @Schema(description = "인터넷 온라인 할인 가격", example = "42900")
     private int internetDiscount;
+
+    @Schema(description = "태그", example = "무제한")
+    private List<TagResponse> tagList;
+
+    @Schema(description = "결합 혜택", example = "가족결합")
+    private List<CommunityBenefitResponse> communityBenefitList;
 
     public InternetPlanDetailResponse(InternetPlan plan, boolean inUse) {
         super(plan, inUse);
@@ -24,5 +32,13 @@ public class InternetPlanDetailResponse extends PlanDetailResponse {
         this.setPlanType("InternetPlan");
         this.velocity = plan.getVelocity();
         this.internetDiscount = plan.getInternetDiscountRate();
+
+        this.tagList = plan.getPlanTags().stream()
+            .map(planTag -> new TagResponse(planTag.getTag()))
+            .toList();
+
+        this.communityBenefitList = plan.getCommunityBenefitList().stream()
+            .map(planCommunity -> new CommunityBenefitResponse(planCommunity.getCommunityBenefit()))
+            .toList();
     }
 }
