4e58a83dcfeabe3652d4ee6593b424076d6b4d81
Yeoeun Yang <yyang78077@naver.com>
2025-06-23 13:39:39 +0900

    fix: 중복 검사 로직 수정

--- src/main/java/com/ureca/uplait/domain/user/repository/BookmarkRepository.java
+++ src/main/java/com/ureca/uplait/domain/user/repository/BookmarkRepository.java
@@ -3,12 +3,14 @@ package com.ureca.uplait.domain.user.repository;
 import com.ureca.uplait.domain.plan.entity.Plan;
 import com.ureca.uplait.domain.user.entity.Bookmark;
 import java.util.Optional;
+
+import com.ureca.uplait.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long>,
     CustomBookmarkRepository {
 
-    boolean existsBookmarkByPlanId(Long planId);
+    boolean existsBookmarkByPlanIdAndUser(Long planId, User user);
 
     Optional<Bookmark> findByUserIdAndPlanId(Long userId, Long planId);
 
